4f591bd6ddcf982f4e2f424d138bfdccf8bc26a3
Alex Newman <17182105+alexnewman42@users.noreply.github.com>
2024-12-16 09:21:42 -0500

    Add skeleton of app

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const appSettings = {
+    dataBaseURL: "https://playground-d63a4-default-rtdb.firebaseio.com/",
+}
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 

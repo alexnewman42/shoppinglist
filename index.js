c79dc22a4480ab6ffe0acaee65d699841deab399
Alex Newman <17182105+alexnewman42@users.noreply.github.com>
2024-12-17 10:12:19 -0500

    Connect app to firebase db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     dataBaseURL: "https://playground-d63a4-default-rtdb.firebaseio.com/",
 }
 
-const inputFieldEl = document.getElementById("input-field");
-const addButtonEl = document.getElementById("add-button");
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    console.log()
-});
+    let inputValue = inputFieldEl.value 
+    console.log(inputValue)
+})
